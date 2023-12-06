d11f963f4d3a681ba112a436b9a08c9e89393dd6
Nga Nguyen <nguyentuyetnga0209@gmail.com>
2023-12-06 13:27:28 +0700

    Add project files.

---
+++ DemoWF001/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DemoWF001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(152, 84);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(213, 54);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Click me !!!";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Touch Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClickMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DemoWF001/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoWF001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            if (sender == btnClickMe)
+            {
+                DialogResult flag = MessageBox.Show("Xin chào, hôm nay trời có đẹp không", "C2208I1", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                MessageBox.Show(flag == DialogResult.Yes ? "bạn chọn YES" : "bạn chọn NO");
+            }
+            else
+            {
+                DialogResult flag = MessageBox.Show("Xin chào, hôm nay trời có đẹp không", "C2208I1", MessageBoxButtons.OKCancel, MessageBoxIcon.Information); ;
+
+                MessageBox.Show(flag == DialogResult.OK ? "bạn chọn OK" : "bạn chọn CANCEL");
+            }
+        }
+    }
+}
